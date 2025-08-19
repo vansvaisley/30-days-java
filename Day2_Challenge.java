7d9467cc1a26cbd9e978b58f221cd37a0d7aac78
Vanshika <vanshi6sept@gmail.com>
2025-08-19 22:14:17 +0530

    Update Day2_Challenge.java
    Swapping of two numbers

--- Day2_Challenge.java
+++ Day2_Challenge.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class swap {
     public static void main(String[] args) {
-        System.out.println(" enter the first number");
+        System.out.println(" Enter the value of a :");
         Scanner input=new Scanner(String.in);
          int a = input.nextInt();
-        System.out.println(" enter the second number");
+        System.out.println(" Enter the value of b :");
         int b = input.nextInt();
         int temp=a;
              a=b;
              b=temp;
-        System.out.println("swapped numbers are :");
+        System.out.println("Swapped numbers are :");
         System.out.println("a is"+ a);
         System.out.println("b is"+ b);
     }
