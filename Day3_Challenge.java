14f71682a965c2b9d1231ace8e6bb0144b0c5d89
Vanshika <vanshi6sept@gmail.com>
2025-08-20 22:23:35 +0530

    Create Day3_Challenge.java
    fahrenheitToCelsius

---
+++ Day3_Challenge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class fahrenheitToCelsius {
+    public static void main(String[] args) {
+        System.out.println("Converting temperature ");
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the temp in fahrenheit : ");
+        float fah= input.nextFloat();
+        float cel = (fah - 32) * 5 / 9;
+        System.out.println(" Temp in celsius is : " + cel + "C");
+        }
+
+}
