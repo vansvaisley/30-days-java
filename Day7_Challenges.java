f5e431cdcf9683a06335dc633d1b8db36ce871d5
Vanshika <vanshi6sept@gmail.com>
2025-08-26 21:13:07 +0530

    Update Day7_Challenges.java
    Number guessing Game

--- Day7_Challenges.java
+++ Day7_Challenges.java
@@ -3,7 +3,7 @@ public class numberGuessingGame {
     public static void main(String[] args) {
         System.out.println("welcome to number guessing game !");
         Scanner input = new Scanner(System.in);
-        int num = 5, guess;
+        int num = 6, guess;
         do {
             System.out.println("please enter the number between 1 to 10 :");
             guess = input.nextInt();
